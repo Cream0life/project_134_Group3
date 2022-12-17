287b8f304054549a79b4fc5b94b81b862d242e7d
Steven Liu <stevenliucan@gmail.com>
2022-12-16 22:11:41 -0500

    phase 3

--- js/validationrom.js
+++ js/validationrom.js
@@ -1,13 +1,12 @@
 function validationrom(){
 
-    var age=document.getElementById("t1").value;
+    var age = document.getElementById("t1").value;
     var student = document.querySelector('input[name="student"]:checked');
     var ttype = document.querySelector('input[name="ttype"]:checked');
 
 
     if(age==""){
         document.getElementById("t1result").innerHTML="(Age is blank)";
-
     }
     else if(age<4 || age>130){
         document.getElementById("t1result").innerHTML="(Age range is not right: 4-130)";
@@ -17,7 +16,6 @@ function validationrom(){
 
     if(student==null){
         document.getElementById("t2result").innerHTML="(You must choose whether you are student or not)";
-
     }
     else   
         document.getElementById("t2result").innerHTML="";
@@ -25,7 +23,6 @@ function validationrom(){
 
     if(ttype==null){
         document.getElementById("t3result").innerHTML="(You must choose one)";
-
     }
     else   
         document.getElementById("t3result").innerHTML="";
@@ -59,3 +56,74 @@ function validationrom(){
     }
 
     }
+
+function validationsurvey(){
+
+    var lname = document.getElementById("ln").value;
+    var fname = document.getElementById("fn").value;
+    var gender = document.querySelector('input[name="gen"]:checked');
+    var age = document.getElementById("age").value;
+    var nat = document.getElementById("nat").value;
+    var email = document.getElementById("em").value;
+    var phone = document.getElementById("ph").value;
+    var way = document.querySelector('select[name="way"]');
+    var checkvalue=true
+    
+
+    if(lname==""){
+        document.getElementById("t1result").innerHTML="(Last Name is blank)";
+        checkvalue=false;
+    }
+    else
+        document.getElementById("t1result").innerHTML="";
+
+    if(fname==""){
+        document.getElementById("t2result").innerHTML="(First Name is blank)";
+        checkvalue=false;
+    }
+    else
+        document.getElementById("t2result").innerHTML="";
+
+    if(gender==null){
+        document.getElementById("t3result").innerHTML="(You must choose one)";
+        checkvalue=false;
+    }
+    else   
+        document.getElementById("t3result").innerHTML="";
+
+    if(age==""){
+        document.getElementById("t4result").innerHTML="(Age is blank)";
+        checkvalue=false;
+    }
+    else
+        document.getElementById("t4result").innerHTML="";
+
+    if(nat==""){
+        document.getElementById("t5result").innerHTML="(Nationality is blank)";
+        checkvalue=false;
+    }
+    else
+        document.getElementById("t5result").innerHTML="";
+
+    if(email==""){
+        document.getElementById("t6result").innerHTML="(Email Address is blank)";
+        checkvalue=false;
+    }
+    else
+        document.getElementById("t6result").innerHTML="";
+    
+    if(phone==""){
+        document.getElementById("t7result").innerHTML="(Phone No. is blank)";
+        checkvalue=false;
+    }
+    else
+        document.getElementById("t7result").innerHTML="";
+
+    if(checkvalue==true){
+        alert("Submited! Thank you for your response.")
+    }
+    else
+        alert("Please try again!");
+}
+
+
